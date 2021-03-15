b9cb1eb547aabf168828c7ff2844749ea3080063
Irvin / Sylver <80391836+IrvinSeneze@users.noreply.github.com>
2021-03-15 16:09:28 +0100

    Vérification : Zone de test V0.0.1
    - Fonction déplacer ("principale") , avec une simulation de la fonction demander et le tableau des coordonnées utilisé dans la grande partie des fonctions
    - Une simulation du Plateau : à copier dans la fonction à tester (Ne pas garder pour la version finale)

---
+++ ProjetTestVerification.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+/*-Prototypes de fonctions-*/
+
+void Deplacer();
+
+/*-Main-*/
+
+int main()
+{
+    Deplacer();
+}
+
+/*-Fonction Demander-*/
+void Deplacer() {
+    int Tab_Coordonnes[4]; // yDepart[0] xDepart[1] yArrive[2] xArrive[3]
+
+    /*-Simulation de la fonction Demander-*/
+
+    Tab_Coordonnes[0] = 1 ; // yDepart
+    Tab_Coordonnes[1] = 1 ; // xDepart
+    Tab_Coordonnes[2] = 1 ; // yArrive
+    Tab_Coordonnes[3] = 1 ; // xArrive
+
+    /*-------------------------------------------------ZONE DE TEST-------------------------------------------------------------*/
+}
+
+void SystèmeDeTableau() {     
+    //NE COPIER SEULEMENT L INTERIEUR DE LA FONCTION ET LA COPIER DANS LA FONCTION A TESTER (fonctionne sans si c'est une  méthode)
+
+    /*-Tableau-*/
+
+    int Plateau[9][9];
+
+    int i, j;
+
+    /*-Les cases non utilisés-*/
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            Plateau[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < 9; i++) {
+        Plateau[0][i] = 99;
+        Plateau[i][0] = 99;
+    }
+
+    /*--Placer les pions ici--*/
+
+    Plateau[4][4] = 01;  //pion qui se déplace soir en 7/4 soit en 8/8 
+    Plateau[5][4] = 11;  //pion pour bloquer verticalement
+    Plateau[6][6] = 14;  //pion pour bloquer diagonalement
+
+    /*-Affichage du Tableau-*/
+
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            //std::cout<<Plateau[i][j]<<" ";
+            printf("%.2i ", Plateau[i][j]);
+        }
+        std::cout << std::endl;
+    }
+}
